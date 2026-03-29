f1eb3da79177bc385a6dafc4d33421826b9f7621
Fernando Alves de Souza <fernando.souza2@estudante.ifgoiano.edu.br>
2026-03-29 16:51:05 -0300

    Remove comments from login process
    Removed comments from the login process in App.java.

--- aula V.2/src/App.java
+++ aula V.2/src/App.java
@@ -4,27 +4,23 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // Obtém a instância Singleton da classe Login
         Login sistemaLogin = Login.getInstancia();
 
         System.out.println("Bem-vindo ao Sistema!");
         System.out.println("=== Tela de Login ===");
 
-        // Solicita as credenciais ao usuário
         System.out.print("Digite o usuário: ");
         String usuarioDigitado = scanner.nextLine();
 
         System.out.print("Digite a senha: ");
         String senhaDigitada = scanner.nextLine();
 
-        // Gera e exibe o código para verificação de bot
         String codigoGerado = sistemaLogin.gerarCodigoVerificador();
         System.out.println("\n[SISTEMA ANTI-BOT]");
         System.out.println("Para prosseguir, digite o seguinte código: " + codigoGerado);
         System.out.print("Código: ");
         String codigoDigitado = scanner.nextLine();
 
-        // Tenta realizar a autenticação
         System.out.println("\nProcessando...");
         boolean sucesso = sistemaLogin.autenticar(usuarioDigitado, senhaDigitada, codigoDigitado, codigoGerado);
 
